2235654618cd829027604a0be6df521d726084df
brynn <brynn@brynndeMacBook-Pro.local>
2022-04-05 20:41:18 +0800

    feat: test

---
+++ egg-example2/app/controller/home.ts
@@ -0,0 +1,27 @@
+import { Controller } from 'egg';
+
+export default class HomeController extends Controller {
+  public async index() {
+    const { ctx } = this;
+    await ctx.render('test.nj');
+  }
+  async login() {
+    const { ctx } = this;
+    ctx.body = {
+      code: 200,
+      data: '!!',
+      msg: 'hello',
+    };
+    // ctx.
+    ctx.cookies.set(ctx.get('origin'), ctx.get('origin'), {
+      // httpOnly: false,
+      // signed: false,
+      maxAge: 200 * 2000,
+      // path: '/',
+      // secure: false,
+      // sameSite: 'none',
+    });
+    ctx.status = 200;
+
+  }
+}
---
+++ egg-example2/config/config.default.ts
@@ -0,0 +1,40 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1649161591926_498';
+
+  // add your egg config in here
+  config.middleware = [];
+
+  // add your special config in here
+  const bizConfig = {
+    security: {
+      csrf: {
+        enable: false,
+        ignore: true,
+      },
+    },
+    view: {
+      defaultViewEngine: 'nunjucks',
+      mapping: {
+        '.tpl': 'nunjucks',
+      },
+    },
+    cors: {
+      origin: ctx => ctx.get('origin'),
+      allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+      credentials: true,
+    },
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig,
+  };
+};
